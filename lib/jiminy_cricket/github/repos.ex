324dae920f5828f2bf68f8f4cfae7f2927f0d3c0
Mauro George <maurogot@gmail.com>
2014-08-24 12:07:44 -0300

    Start of JiminyCricket.Github.Repos.fetch

--- lib/jiminy_cricket/github/repos.ex
+++ lib/jiminy_cricket/github/repos.ex
@@ -3,7 +3,15 @@ defmodule JiminyCricket.Github.Repos do
   @github_api_url Application.get_env(:jiminy_cricket, :github_api_url)
   @organization_name Application.get_env(:jiminy_cricket, :organization_name)
 
+  def fetch do
+    url |> HTTPoison.get |> handle_response
+  end
+
   def url do
     "#{@github_api_url}/orgs/#{@organization_name}/repos"
+  end
+
+  def handle_response(%{status_code: 200, body: body}) do
+    JSEX.decode body
   end
 end
