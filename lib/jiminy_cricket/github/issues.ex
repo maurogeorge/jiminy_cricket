3b509239f58e961f0c64607ecd6394bc7dff0bfd
Mauro George <maurogot@gmail.com>
2014-09-19 14:33:24 -0300

    Filter iteractions that dont have interaction in a long time

--- lib/jiminy_cricket/github/issues.ex
+++ lib/jiminy_cricket/github/issues.ex
@@ -7,6 +7,13 @@ defmodule JiminyCricket.Github.Issues do
     |> build_issues
   end
 
+  def last_iteration_happened_more_than_two_weeks_ago?(issue) do
+    {:ok, issue_date } = Timex.DateFormat.parse(issue["updated_at"], "{ISOz}")
+    today = Timex.Date.now
+    weeks = Timex.Date.diff(today, issue_date, :weeks)
+    weeks <= -2
+  end
+
   defp parse_issue_url(repo), do: Regex.replace(~r({/number}), repo["issues_url"], "")
 
   defp get_issues(issue_url) do
