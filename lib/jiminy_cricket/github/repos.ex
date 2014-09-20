b31095efb375d8144da36dde702cb917b571ed5a
Mauro George <maurogot@gmail.com>
2014-09-20 18:57:59 -0300

    Add access token on Repos

--- lib/jiminy_cricket/github/repos.ex
+++ lib/jiminy_cricket/github/repos.ex
@@ -2,13 +2,14 @@ defmodule JiminyCricket.Github.Repos do
 
   @github_api_url Application.get_env(:jiminy_cricket, :github_api_url)
   @organization_name Application.get_env(:jiminy_cricket, :organization_name)
+  @github_access_token System.get_env "GITHUB_ACCESS_TOKEN"
 
   def fetch do
     url |> HTTPoison.get |> handle_response |> build_repos
   end
 
   defp url do
-    "#{@github_api_url}/orgs/#{@organization_name}/repos"
+    "#{@github_api_url}/orgs/#{@organization_name}/repos?access_token=#{@github_access_token}"
   end
 
   defp handle_response(%{status_code: 200, body: body}) do
