052196b964cf47e3f72c1b12ea4648d2b3f0a5e7
Mauro George <maurogot@gmail.com>
2014-09-20 19:34:57 -0300

    Move github_access_token to a function over a module attribute
    When using JiminyCricket.Github.Url.append_access_token
    always the return of @github_access_token is nil.
    When changed to a function the expected behavior is
    executed.

--- lib/jiminy_cricket/github/url.ex
+++ lib/jiminy_cricket/github/url.ex
@@ -1,8 +1,10 @@
 defmodule JiminyCricket.Github.Url do
 
-  @github_access_token System.get_env "GITHUB_ACCESS_TOKEN"
-
   def append_access_token(url) do
-    "#{url}?access_token=#{@github_access_token}"
+    "#{url}?access_token=#{github_access_token}"
+  end
+
+  defp github_access_token do
+    System.get_env("GITHUB_ACCESS_TOKEN")
   end
 end
