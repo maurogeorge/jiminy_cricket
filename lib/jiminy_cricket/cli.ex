48e364ff3ee64d04f5e251d779da2555013f6281
Mauro George <maurogot@gmail.com>
2014-09-20 19:49:39 -0300

    Add to JiminyCricket.CLI to create comment

--- lib/jiminy_cricket/cli.ex
+++ lib/jiminy_cricket/cli.ex
@@ -5,5 +5,6 @@ defmodule JiminyCricket.CLI do
     |> Enum.map(&JiminyCricket.Github.Issues.fetch(&1))
     |> List.flatten
     |> Enum.filter(&JiminyCricket.Github.Issues.last_iteration_happened_more_than_two_weeks_ago?(&1))
+    |> Enum.map(&JiminyCricket.Github.Issues.create_comment(&1))
   end
 end
